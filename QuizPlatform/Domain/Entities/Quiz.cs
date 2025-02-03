f80275364a366bbcdb57b01ac0788b6e64255365
minayev_aleksandr <minayev_aleksandr@pro-compliance.ru>
2025-02-03 18:01:56 +0300

    Added Domain layer and some entities

---
+++ QuizPlatform/Domain/Entities/Quiz.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Quiz : BaseAuditableEntity
+{
+    [Required]
+    [MaxLength(200)]
+    public string? Tittle { get; set; }
+}
